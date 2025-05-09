786b034e8e8d18717497b3be391b080932322c09
areebaawan <areeba.awan2468@gmail.com>
2025-05-09 12:33:32 +0500

    Rename test class to TestFlaskApp for pytest detection

---
+++ test_main_app.py
@@ -0,0 +1,14 @@
+# test_main_app.py
+import pytest
+from main_app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"CI/CD Dashboard" in response.data
